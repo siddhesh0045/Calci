735e7fc132d0f97194a72bdd1ff6d5b5908c7fdd
Siddheshwar Ghuge <siddheshwarg4u@gmail.com>
2023-08-30 19:20:13 +0530

    code Updated For making easy to deploy

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ class Calculator {
         this.getDisplayNumber(this.currentOperand)
       if (this.operation != null) {
         this.previousOperandTextElement.innerText =
-          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+          `${this.getDisplayNumber(this.previousOperand)}  ${this.operation}`
       } else {
         this.previousOperandTextElement.innerText = ''
       }
